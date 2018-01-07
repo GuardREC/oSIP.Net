e2f9685c03df5447b7fa3558dca7f736cdb16af7
Rune Vistnes <rv@guardrec.no>
2018-01-07 20:08:21 +0100

    Implemented AllowHeader

---
+++ src/oSIP.Net/AllowHeader.cs
@@ -0,0 +1,20 @@
+﻿namespace oSIP.Net
+{
+    public class AllowHeader : ValueHeaderBase
+    {
+        public AllowHeader()
+        {
+        }
+
+        internal unsafe AllowHeader(osip_content_length_t* native, bool isOwner)
+            :
+            base(native, isOwner)
+        {
+        }
+
+        public static AllowHeader Parse(string str)
+        {
+            return Parse<AllowHeader>(str);
+        }
+    }
+}
