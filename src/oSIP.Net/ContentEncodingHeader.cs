a40246b1ed221956e8cedc5a73cc2c9d5e04696f
Rune Vistnes <rv@guardrec.no>
2018-01-09 20:30:39 +0100

    Implemented ContentEncodingHeader

---
+++ src/oSIP.Net/ContentEncodingHeader.cs
@@ -0,0 +1,20 @@
+﻿namespace oSIP.Net
+{
+    public class ContentEncodingHeader : ValueHeaderBase
+    {
+        public ContentEncodingHeader()
+        {
+        }
+
+        internal unsafe ContentEncodingHeader(osip_content_length_t* native, bool isOwner)
+            :
+            base(native, isOwner)
+        {
+        }
+
+        public static ContentEncodingHeader Parse(string str)
+        {
+            return Parse<ContentEncodingHeader>(str);
+        }
+    }
+}
