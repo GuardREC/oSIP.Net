907dcfbe4ff06e2098f3ad0d36cf9258b8f2250d
Rune Vistnes <rv@guardrec.no>
2018-01-09 22:00:52 +0100

    Implemented RouteHeader

---
+++ src/oSIP.Net/RouteHeader.cs
@@ -0,0 +1,29 @@
+﻿namespace oSIP.Net
+{
+    public class RouteHeader : NameAddressHeaderBase
+    {
+        public RouteHeader()
+        {
+        }
+
+        internal unsafe RouteHeader(osip_from_t* native, bool isOwner)
+            :
+            base(native, isOwner)
+        {
+        }
+
+        public static RouteHeader Parse(string str)
+        {
+            return Parse<RouteHeader>(str);
+        }
+
+        public unsafe RouteHeader DeepClone()
+        {
+            return DeepClone(ptr =>
+            {
+                var native = (osip_from_t*) ptr.ToPointer();
+                return new RouteHeader(native, true);
+            });
+        }
+    }
+}
