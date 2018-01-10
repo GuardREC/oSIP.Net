27e5080c060acb0d29ffdfd719cf7fc1f50375ec
Rune Vistnes <rv@guardrec.no>
2018-01-10 15:04:24 +0100

    Implemented ProxyAuthenticationInfoHeader

---
+++ src/oSIP.Net/ProxyAuthenticationInfoHeader.cs
@@ -0,0 +1,29 @@
+﻿namespace oSIP.Net
+{
+    public class ProxyAuthenticationInfoHeader : AuthenticationInfoHeaderBase
+    {
+        public ProxyAuthenticationInfoHeader()
+        {
+        }
+
+        internal unsafe ProxyAuthenticationInfoHeader(osip_authentication_info_t* native, bool isOwner)
+            :
+            base(native, isOwner)
+        {
+        }
+
+        public static ProxyAuthenticationInfoHeader Parse(string str)
+        {
+            return Parse<ProxyAuthenticationInfoHeader>(str);
+        }
+
+        public unsafe ProxyAuthenticationInfoHeader DeepClone()
+        {
+            return DeepClone(ptr =>
+            {
+                var native = (osip_authentication_info_t*) ptr.ToPointer();
+                return new ProxyAuthenticationInfoHeader(native, true);
+            });
+        }
+    }
+}
