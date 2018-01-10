08a42cfabc3f5bada108bcaa5947425fb3210547
Rune Vistnes <rv@guardrec.no>
2018-01-10 11:41:43 +0100

    Implemented AlertInfoHeader

---
+++ src/oSIP.Net/AlertInfoHeader.cs
@@ -0,0 +1,29 @@
+﻿namespace oSIP.Net
+{
+    public class AlertInfoHeader : ValueParametersHeaderBase
+    {
+        public AlertInfoHeader()
+        {
+        }
+
+        internal unsafe AlertInfoHeader(osip_call_info_t* native, bool isOwner)
+            :
+            base(native, isOwner)
+        {
+        }
+
+        public static AlertInfoHeader Parse(string str)
+        {
+            return Parse<AlertInfoHeader>(str);
+        }
+
+        public unsafe AlertInfoHeader DeepClone()
+        {
+            return DeepClone(ptr =>
+            {
+                var native = (osip_call_info_t*) ptr.ToPointer();
+                return new AlertInfoHeader(native, true);
+            });
+        }
+    }
+}
