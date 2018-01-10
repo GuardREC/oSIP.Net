90e84d3806687ed204d149729055d47a1e726e7e
Rune Vistnes <rv@guardrec.no>
2018-01-10 08:19:58 +0100

    Implemented RecordRouteHeader

---
+++ src/oSIP.Net/RecordRouteHeader.cs
@@ -0,0 +1,29 @@
+﻿namespace oSIP.Net
+{
+    public class RecordRouteHeader: NameAddressHeaderBase
+    {
+        public RecordRouteHeader()
+        {
+        }
+
+        internal unsafe RecordRouteHeader(osip_from_t* native, bool isOwner)
+            :
+            base(native, isOwner)
+        {
+        }
+
+        public static RecordRouteHeader Parse(string str)
+        {
+            return Parse<RecordRouteHeader>(str);
+        }
+
+        public unsafe RecordRouteHeader DeepClone()
+        {
+            return DeepClone(ptr =>
+            {
+                var native = (osip_from_t*) ptr.ToPointer();
+                return new RecordRouteHeader(native, true);
+            });
+        }
+    }
+}
