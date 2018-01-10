354092ec858029a4ce39b41be004834568afea52
Rune Vistnes <rv@guardrec.no>
2018-01-10 11:47:06 +0100

    Implemented ErrorInfoHeader

---
+++ src/oSIP.Net/ErrorInfoHeader.cs
@@ -0,0 +1,29 @@
+﻿namespace oSIP.Net
+{
+    public class ErrorInfoHeader : ValueParametersHeaderBase
+    {
+        public ErrorInfoHeader()
+        {
+        }
+
+        internal unsafe ErrorInfoHeader(osip_call_info_t* native, bool isOwner)
+            :
+            base(native, isOwner)
+        {
+        }
+
+        public static ErrorInfoHeader Parse(string str)
+        {
+            return Parse<ErrorInfoHeader>(str);
+        }
+
+        public unsafe ErrorInfoHeader DeepClone()
+        {
+            return DeepClone(ptr =>
+            {
+                var native = (osip_call_info_t*) ptr.ToPointer();
+                return new ErrorInfoHeader(native, true);
+            });
+        }
+    }
+}
