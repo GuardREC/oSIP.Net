57f2e2709a8f530223a5ce5fdebc196323746a44
Rune Vistnes <rv@guardrec.no>
2018-01-10 09:43:35 +0100

    Implemented ContactHeader

---
+++ src/oSIP.Net/ContactHeader.cs
@@ -0,0 +1,29 @@
+﻿namespace oSIP.Net
+{
+    public class ContactHeader : NameAddressHeaderBase
+    {
+        public ContactHeader()
+        {
+        }
+
+        internal unsafe ContactHeader(osip_from_t* native, bool isOwner)
+            :
+            base(native, isOwner)
+        {
+        }
+
+        public static ContactHeader Parse(string str)
+        {
+            return Parse<ContactHeader>(str);
+        }
+
+        public unsafe ContactHeader DeepClone()
+        {
+            return DeepClone(ptr =>
+            {
+                var native = (osip_from_t*) ptr.ToPointer();
+                return new ContactHeader(native, true);
+            });
+        }
+    }
+}
