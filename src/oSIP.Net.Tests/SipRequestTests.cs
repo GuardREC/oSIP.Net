4867ecd7d9ba6ef07aeb6136d3dea78475530d16
Håvard Lian <hl@guardrec.no>
2018-04-11 14:02:57 +0200

    Extract common parse logic. Add TryCopyTo byte[]

--- src/oSIP.Net.Tests/SipRequestTests.cs
+++ src/oSIP.Net.Tests/SipRequestTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System;
+using System.Text;
 using NUnit.Framework;
 
 namespace oSIP.Net.Tests
@@ -71,6 +72,84 @@ namespace oSIP.Net.Tests
                     "Content-Length:    12\r\n" +
                     "\r\n" +
                     "Hello world!"));
+            }
+        }
+
+        [Test]
+        public void Shall_byteify_request()
+        {
+            using (var request = new SipRequest())
+            {
+                request.Version = "SIP/2.0";
+                request.Method = "INVITE";
+                request.Vias.Add(ViaHeader.Parse("SIP/2.0/UDP foo.bar.com"));
+                request.RecordRoutes.Add(RecordRouteHeader.Parse("Tommy Atkins <sip:tommy.atkins@abc.com>"));
+                request.Routes.Add(RouteHeader.Parse("John Doe <sip:john.doe@abc.com>"));
+                request.RequestUri = SipUri.Parse("sip:john.smith@abc.com");
+                request.From = FromHeader.Parse("John Smith <sip:john.smith@abc.com>");
+                request.To = ToHeader.Parse("Joe Shmoe <sip:joe.shmoe@abc.com>");
+                request.CallId = CallIdHeader.Parse("1@foo.bar.com");
+                request.CSeq = CSeqHeader.Parse("1 INVITE");
+                request.Contacts.Add(ContactHeader.Parse("Prisoner X <sip:prisoner.x@abc.com>"));
+                request.Authorizations.Add(AuthorizationHeader.Parse("Digest username=\"Alice\""));
+                request.WwwAuthenticates.Add(WwwAuthenticateHeader.Parse("Digest realm=\"abc.com\""));
+                request.ProxyAuthenticates.Add(ProxyAuthenticateHeader.Parse("Digest realm=\"xyz.com\""));
+                request.ProxyAuthorizations.Add(ProxyAuthorizationHeader.Parse("Digest username=\"Bob\""));
+                request.CallInfos.Add(CallInfoHeader.Parse("<http://www.abc.com/photo.png>;purpose=icon"));
+                request.ContentType = ContentTypeHeader.Parse("text/plain");
+                request.MimeVersion = MimeVersionHeader.Parse("1.0");
+                request.Allows.Add(AllowHeader.Parse("INVITE, ACK, BYE"));
+                request.ContentEncodings.Add(ContentEncodingHeader.Parse("deflate"));
+                request.AlertInfos.Add(AlertInfoHeader.Parse("<http://www.abc.com/sound.wav>"));
+                request.ErrorInfos.Add(ErrorInfoHeader.Parse("<sip:not-in-service@abc.com>"));
+                request.Accepts.Add(AcceptHeader.Parse("application/sdp"));
+                request.AcceptEncodings.Add(AcceptEncodingHeader.Parse("gzip"));
+                request.AcceptLanguages.Add(AcceptLanguageHeader.Parse("en"));
+                request.AuthenticationInfos.Add(AuthenticationInfoHeader.Parse("nextnonce=\"abc\""));
+                request.ProxyAuthenticationInfos.Add(ProxyAuthenticationInfoHeader.Parse("nextnonce=\"def\""));
+                request.OtherHeaders.Add(new GenericHeader("P-Asserted-Identity", "sip:alan.smithee@abc.com"));
+                request.Bodies.Add(SipBody.Parse("Hello world!"));
+
+                var buffer = new byte[ushort.MaxValue];
+                var success = request.TryCopyTo(buffer, 0, out int length);
+                
+                Assert.That(success, Is.True);
+
+                using (var request2 = SipMessage.Parse(new ArraySegment<byte>(buffer, 0, length)))
+                {
+                    Assert.That(request2.ToString(), Is.EqualTo(
+                        "INVITE sip:john.smith@abc.com SIP/2.0\r\n" +
+                        "Via: SIP/2.0/UDP foo.bar.com\r\n" +
+                        "Record-Route: Tommy Atkins <sip:tommy.atkins@abc.com>\r\n" +
+                        "Route: John Doe <sip:john.doe@abc.com>\r\n" +
+                        "From: John Smith <sip:john.smith@abc.com>\r\n" +
+                        "To: Joe Shmoe <sip:joe.shmoe@abc.com>\r\n" +
+                        "Call-ID: 1@foo.bar.com\r\n" +
+                        "CSeq: 1 INVITE\r\n" +
+                        "Contact: Prisoner X <sip:prisoner.x@abc.com>\r\n" +
+                        "Authorization: Digest username=\"Alice\"\r\n" +
+                        "WWW-Authenticate: Digest realm=\"abc.com\"\r\n" +
+                        "Proxy-Authenticate: Digest realm=\"xyz.com\"\r\n" +
+                        "Proxy-Authorization: Digest username=\"Bob\"\r\n" +
+                        "Call-Info: <http://www.abc.com/photo.png>;purpose=icon\r\n" +
+                        "Content-Type: text/plain\r\n" +
+                        "Mime-Version: 1.0\r\n" +
+                        "Allow: INVITE\r\n" +
+                        "Allow: ACK\r\n" +
+                        "Allow: BYE\r\n" +
+                        "Content-Encoding: deflate\r\n" +
+                        "Alert-Info: <http://www.abc.com/sound.wav>\r\n" +
+                        "Error-Info: <sip:not-in-service@abc.com>\r\n" +
+                        "Accept: application/sdp\r\n" +
+                        "Accept-Encoding: gzip\r\n" +
+                        "Accept-Language: en\r\n" +
+                        "Authentication-Info: nextnonce=\"abc\"\r\n" +
+                        "Proxy-Authentication-Info: nextnonce=\"def\"\r\n" +
+                        "P-asserted-identity: sip:alan.smithee@abc.com\r\n" +
+                        "Content-Length:    12\r\n" +
+                        "\r\n" +
+                        "Hello world!"));
+                }
             }
         }
 
